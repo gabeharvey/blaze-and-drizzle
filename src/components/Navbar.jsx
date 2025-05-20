e579396bc390c07e21353bb2d31f2f060992223b
Gabe Harvey <harvey7@gmail.com>
2025-05-19 19:38:00 -0500

    Navbar.jsx Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -262,12 +262,15 @@ const Navbar = () => {
             </Box>
             <Box mt={{ base: '-.1rem', md: '-.2rem' }} w="100%" zIndex="1">
                 <Image
-                src="/slant-divider.svg"
-                alt="Navbar bottom border"
-                w="100%"
-                h="auto"
-                display="block"
-                />
+  src="/slant-divider.svg"
+  alt="Navbar bottom border"
+  w="100%"
+  h="auto"
+  display="block"
+  m="0"
+  p="0"
+/>
+
             </Box>
         </Box>
     );
