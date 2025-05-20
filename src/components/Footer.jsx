51483241974b8c1fd710e56ec9e1e5e3eadb1f6f
Gabe Harvey <harvey7@gmail.com>
2025-05-19 21:22:53 -0500

    Footer.jsx Resize

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,74 +2,74 @@ import { Box, Flex, Text, Image } from '@chakra-ui/react';
 
 function Footer() {
   return (
-<Box
-  as="footer"
-  bg="#000000"
-  py="3rem"
-  px={0}  // No padding here to avoid clipping SVG
-  fontFamily="'Bungee', sans-serif"
-  color="#F6E0B3"
-  mt="auto"
-  position="relative"
-  zIndex={1}
-  overflow="hidden"
-  border="none"
->
-  <Box
-    position="absolute"
-    top="0"
-    left="50%"
-    width="100vw"
-    minWidth="100vw"
-    overflow="hidden"
-    zIndex={2}
-    pointerEvents="none"
-    userSelect="none"
-    style={{ transform: 'translateX(-50%) scaleY(-1) scaleX(-1)' }}
-  >
-    <Image
-      src="/grunge-divider-1.svg"
-      alt="Footer top border"
-      width="140vw"
-      height="auto"
-      display="block"
-      transform="scaleY(0.999)"
-      transformOrigin="top"
-    />
-  </Box>
-
-  <Flex
-    justify="center"
-    alignItems="center"
-    position="relative"
-    zIndex={3}
-    pt="5rem"
-    pb="0"
-    px="2rem"  // Padding only inside content, not on SVG container
-    textAlign="center"
-    flexWrap="wrap"
-  >
-    <Text
-      fontSize={['md', 'lg']}
-      fontWeight="bold"
-      letterSpacing="wider"
-      whiteSpace={{ base: 'normal', md: 'nowrap' }}
-      overflow="visible"
+    <Box
+      as="footer"
+      bg="#000000"
+      px={0}
+      fontFamily="'Bungee', sans-serif"
+      color="#F6E0B3"
+      mt="auto"
+      position="relative"
+      zIndex={1}
+      overflow="hidden"
+      border="none"
+      // removed py here
     >
-      ©2025 Blaze &{' '}
       <Box
-        as="span"
-        fontFamily="'Nosifer', cursive"
-        color="#F6E0B3"
-        display="inline"
+        position="relative"
+        width="100vw"
+        overflow="hidden"
+        zIndex={2}
+        height="auto"
+        pointerEvents="none"
+        userSelect="none"
+        style={{ transform: 'scaleY(-1)' }} // keep flip here
+        mb="-3rem" // add negative margin bottom to pull svg down
       >
-        Drizzle
-      </Box>{' '}
-      LLC
-    </Text>
-  </Flex>
-</Box>
+        <Image
+          src="/grunge-divider-1.svg"
+          alt="Footer top border"
+          width="120vw"
+          maxWidth="none"
+          marginLeft="-10vw"
+          height="auto"
+          objectFit="cover"
+          display="block"
+        />
+      </Box>
 
+      <Flex
+        justify="center"
+        alignItems="center"
+        position="relative"
+        zIndex={3}
+        pt="3rem" // add padding top here below flipped SVG
+        pb="0"
+        px="2rem"
+        textAlign="center"
+        flexWrap="wrap"
+      >
+        <Text
+          fontSize={['md', 'lg']}
+          fontWeight="bold"
+          letterSpacing="wider"
+          whiteSpace={{ base: 'normal', md: 'nowrap' }}
+          overflow="visible"
+          mb="2rem"
+        >
+          ©2025 Blaze &{' '}
+          <Box
+            as="span"
+            fontFamily="'Nosifer', cursive"
+            color="#F6E0B3"
+            display="inline"
+          >
+            Drizzle
+          </Box>{' '}
+          LLC
+        </Text>
+      </Flex>
+    </Box>
   );
 }
 
